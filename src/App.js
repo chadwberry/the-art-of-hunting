eb06c4ee76be4520f0bb8dfe3acdf9b6ba51ba4d
Chad Berry <BERRY@Chads-MacBook-Air.local>
2017-03-06 17:26:02 -0800

    added navigation bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import MdMenu from 'react-icons/lib/md/menu';
+import './app.styles.scss';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Hello World</h1>
+      <div className="appContainer">
+        <div className="navigationBar">
+          <div className="logoContainer">
+            <div style={{ backgroundImage: 'url(/images/TheArtofHuntingLogo.png)' }}
+                 className="navLogo"/>
+            <h1 className="mainHeader">The ART of HUNTING</h1>
+          </div>
+          <MdMenu className="menuIcon"/>
+        </div>
       </div>
     );
   }
